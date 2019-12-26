35a7b45abdff2dfd13b997111054b0a62d3e541b
SayKo-gpu <59257028+SayKo-gpu@users.noreply.github.com>
2019-12-26 15:27:48 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+
+
+
+
+bot.on('ready', () => {
+  console.log('Bot Connected')
+})
+
+
+
+bot.login('NjU5NDUzMDUxODk1MTUyNjgw.XgTBdw.l-UW_QZZ9Yi0-dMuATSgDLVljU0')
